6dcd79fe43c0a690921ccd0fd48aa503fc6867a1
kemiz <kemimeister@gmail.com>
2015-08-28 13:11:07 +0100

    clean up

--- package_installer_plugin/service_tasks.py
+++ package_installer_plugin/service_tasks.py
@@ -7,22 +7,23 @@ from cloudify import ctx
 from cloudify import exceptions
 from cloudify.decorators import operation
 
+
 @operation
-def start_service(service_name):
+def start_service(service_name, **_):
     start_command = START_SERVICE + service_name
     ctx.logger.info('Running command: {0}'.format(start_command))
     run(start_command)
 
 
 @operation
-def stop_service(service_name):
+def stop_service(service_name, **_):
     stop_command = STOP_SERVICE + service_name
     ctx.logger.info('Running command: {0}'.format(stop_command))
     run(stop_command)
 
 
 @operation
-def restart_service(service_name):
+def restart_service(service_name, **_):
     stop_command = RESTART_SERVICE + service_name
     ctx.logger.info('Running command: {0}'.format(stop_command))
     run(stop_command)
