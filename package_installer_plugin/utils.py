bcdb46493e9f7d7274a40642fe69796beb0da864
kemiz <kemimeister@gmail.com>
2015-09-04 12:41:17 +0100

    utils cleanup

--- package_installer_plugin/utils.py
+++ package_installer_plugin/utils.py
@@ -11,21 +11,15 @@ from cloudify import exceptions
 def run(command):
 
     command_as_list = command.split()
-    execution = None
-    output = None
 
     try:
         execution = subprocess.check_output(command_as_list)
     except Exception as e:
         raise exceptions.NonRecoverableError(
-            'Unable to run command. Error {0}'.format(execution.returncode))
+            'Unable to run command. Error: {0}'.format(str(e)))
     finally:
-        if execution.returncode != 0:
-            raise exceptions.RecoverableError(
-                'Unable to run command. Error: {0}'.format(execution))
         ctx.logger.info(
-            'RAN: {0}. OUT: {1}. Code: {2}.'.format(
-                command, output, execution.returncode))
+            'RAN: {0}. Code: 0.'.format(command))
     return execution
 
 
