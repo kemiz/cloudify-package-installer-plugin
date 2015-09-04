8e7577c3a76ef7433d836956f843a3718ff1b5af
kemiz <kemimeister@gmail.com>
2015-09-04 10:00:02 +0100

    tomcat plugin impl

--- package_installer_plugin/service_tasks.py
+++ package_installer_plugin/service_tasks.py
@@ -8,30 +8,24 @@ from cloudify.decorators import operation
 
 
 @operation
-def start_service(service_name, custom_command, **_):
+def start_service(service_name, **_):
     start_command = START_SERVICE_COMMAND + service_name
-    if custom_command is not None:
-        start_command = custom_command
     ctx.logger.info('Starting service: ' + service_name)
     ctx.logger.info('Running command: {0}'.format(start_command))
     run(start_command)
 
 
 @operation
-def stop_service(service_name, custom_command, **_):
+def stop_service(service_name, **_):
     stop_command = STOP_SERVICE_COMMAND + service_name
-    if custom_command is not None:
-        stop_command = custom_command
     ctx.logger.info('Stopping service: ' + service_name)
     ctx.logger.info('Running command: {0}'.format(stop_command))
     run(stop_command)
 
 
 @operation
-def restart_service(service_name, custom_command, **_):
+def restart_service(service_name, **_):
     restart_command = RESTART_SERVICE_COMMAND + service_name
-    if custom_command is not None:
-        restart_command = custom_command
     ctx.logger.info('Restarting service: ' + service_name)
     ctx.logger.info('Running command: {0}'.format(restart_command))
     run(restart_command)
