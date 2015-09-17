81464681c553c603deb21bf4aad9bfa15a336d4c
kemiz <kemimeister@gmail.com>
2015-09-17 18:47:41 +0100

    correct node type

--- package_installer_workflows/service_workflows.py
+++ package_installer_workflows/service_workflows.py
@@ -10,21 +10,25 @@ from package_installer_plugin.constants import *
 def start_service(**_):
 
     """ Starts a service on a given node """
+    ctx.logger.info("Starting service...")
 
     for node in ctx.nodes:
-        for node_instance in node.instances:
-            ctx.logger.info("Starting service {0}".format(node_instance))
-            plugin_operation_path = SERVICE_COMMANDS + 'start_service'
-            node_instance.execute_operation(plugin_operation_path, kwargs={'service_name': parameters.service_name})
+        if node.id == parameters.service_name:
+            for node_instance in node.instances:
+                ctx.logger.info("Starting service {0}".format(node_instance))
+                plugin_operation_path = SERVICE_COMMANDS + 'start_service'
+                node_instance.execute_operation(plugin_operation_path, kwargs={'service_name': parameters.service_name})
 
 
 @workflow
 def stop_service(**_):
 
     """ stops a service on a given node """
+    ctx.logger.info("Stopping service...")
 
     for node in ctx.nodes:
-        for node_instance in node.instances:
-            ctx.logger.info("Stopping service {0}".format(node_instance))
-            plugin_operation_path = SERVICE_COMMANDS + 'stop_service'
-            node_instance.execute_operation(plugin_operation_path, kwargs={'service_name': parameters.service_name})
+        if node.id == parameters.service_name:
+            for node_instance in node.instances:
+                ctx.logger.info("Stopping service {0}".format(node_instance))
+                plugin_operation_path = SERVICE_COMMANDS + 'stop_service'
+                node_instance.execute_operation(plugin_operation_path, kwargs={'service_name': parameters.service_name})
